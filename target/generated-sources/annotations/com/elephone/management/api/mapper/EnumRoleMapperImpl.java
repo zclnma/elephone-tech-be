c10eb5d7de53c0d9c2afd813cd1ad7047bf98b6f
lionel <zikai.a.zhao@accenture.com>
2020-11-27 15:39:47 +1100

    6

--- target/generated-sources/annotations/com/elephone/management/api/mapper/EnumRoleMapperImpl.java
+++ target/generated-sources/annotations/com/elephone/management/api/mapper/EnumRoleMapperImpl.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-11-27T12:28:37+1100",
-    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 1.8.0_211 (Oracle Corporation)"
+    date = "2020-11-27T15:36:59+1100",
+    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 11.0.4 (Oracle Corporation)"
 )
 @Component
 public class EnumRoleMapperImpl implements EnumRoleMapper {
