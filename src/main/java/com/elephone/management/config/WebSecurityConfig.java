06701778ef3e61720ea67b99b66884707945df36
Lionel ZHAO <13450508+zclnma@users.noreply.github.com>
2021-02-05 13:26:38 +1100

    fix

--- src/main/java/com/elephone/management/config/WebSecurityConfig.java
+++ src/main/java/com/elephone/management/config/WebSecurityConfig.java
@@ -36,7 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.cors().and().authorizeRequests(authorize ->
+        http.csrf().disable().cors().and().authorizeRequests(authorize ->
                 authorize.anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(jwtConfigurer ->
