f5ba8b0785ce3fa53efaca7d1ed4fcbf08c318d5
Lionel ZHAO <13450508+zclnma@users.noreply.github.com>
2021-09-28 13:19:32 +1000

    fix sql

--- src/main/resources/db/migration/V2.3__add_member.sql
+++ src/main/resources/db/migration/V2.3__add_member.sql
@@ -1,4 +1,2 @@
-alter table elephone.public.transaction
-    alter column membership type BOOLEAN;
-alter table elephone.public.transaction
-    alter column notification type BOOLEAN;
+alter table elephone.public.transaction add column membership boolean default false ;
+alter table elephone.public.transaction add column notification boolean default false;
